f7d780a63c1f0fd756d7dcd3e0b13326974f87fc
g3anderson <gfernando3.a@gmail.com>
2025-08-06 10:58:44 -0300

    Commit inicial, início do projeto.

---
+++ src/main/java/com/dohms/audiovisualController/AudiovisualControllerApplication.java
@@ -0,0 +1,13 @@
+package com.dohms.audiovisualController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudiovisualControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudiovisualControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dohms/audiovisualController/AudiovisualControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dohms.audiovisualController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudiovisualControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
